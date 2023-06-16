b456cfe4aded5ccf8da05716c01acaab1cb38c4f
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-06-16 10:26:12 +0800

    Initial TextView

--- app/src/main/java/com/suhun/threadmessagehandler/MainActivity.java
+++ app/src/main/java/com/suhun/threadmessagehandler/MainActivity.java
@@ -4,14 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private String tag = MainActivity.class.getSimpleName();
+    private TextView showResult1, getShowResult2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initview();
+    }
+    private void initview(){
+        showResult1 = findViewById(R.id.result1);
+        showResult1 = findViewById(R.id.result2);
     }
     public void runThread1Fun(View view){
 
